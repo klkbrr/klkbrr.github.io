867ffd6f6d67ca273a7637370d88fc62c90697a6
klkbrr <94877872+klkbrr@users.noreply.github.com>
2022-03-18 14:07:50 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ function estableceNombreUsuario() {
       estableceNombreUsuario();
     } else {
       localStorage.setItem('nombre', miNombre);
-      miTitulo.innerHTML = 'Mozilla is genial, ' + miNombre;
+      miTitulo.innerHTML = 'Este pana es un Mamabicho:, ' + miNombre;
     }
   }
 
